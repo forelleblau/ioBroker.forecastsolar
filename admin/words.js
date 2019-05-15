8df36dbd1e490826839c9ef4845de1a5b55cc9df
forelleblau <marceladam@gmx.ch>
2019-05-15 21:59:09 +0200

    v001

--- admin/words.js
+++ admin/words.js
@@ -2,40 +2,88 @@
 'use strict';
 
 systemDictionary = {
-    'template adapter settings': {
-        'en': 'Adapter settings for template',
-        'de': 'Adaptereinstellungen für template',
-        'ru': 'Настройки адаптера для template',
-        'pt': 'Configurações do adaptador para template',
-        'nl': 'Adapterinstellingen voor template',
-        'fr': "Paramètres d'adaptateur pour template",
-        'it': "Impostazioni dell'adattatore per template",
-        'es': 'Ajustes del adaptador para template',
-        'pl': 'Ustawienia adaptera dla template',
-        'zh-cn': 'template的适配器设置'
-    },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
-        'ru': 'Опция 1',
-        'pt': 'Opção 1',
-        'nl': 'Optie 1',
-        'fr': 'Option 1',
-        'it': 'opzione 1',
-        'es': 'Opción 1',
-        'pl': 'opcja 1',
-        'zh-cn': '选项1'
-    },
-    'option2': {
-        'en': 'option2',
-        'de': 'Option 2',
-        'ru': 'option2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'zh-cn': '选项2'
-    }
+  'Location': {
+    'en': 'Location',
+    'de': 'Standort',
+    'ru': 'Location',
+    'pt': 'Location',
+    'nl': 'Location',
+    'fr': "Location",
+    'it': "Locazione",
+    'es': 'Location',
+    'pl': 'Location',
+    'zh-cn': 'Location'
+  },
+  'latitude': {
+    'en': 'Latitude',
+    'de': 'Geogr. Breite',
+    'ru': 'Latitude',
+    'pt': 'Latitude',
+    'nl': 'Latitude',
+    'fr': 'Latitude',
+    'it': 'Latitude',
+    'es': 'Latitude',
+    'pl': 'Latitude',
+    'zh-cn': 'Latitude'
+  },
+  'longitude': {
+    'en': 'Longitude',
+    'de': 'Geogr. Länge',
+    'ru': 'Longitude',
+    'pt': 'Longitude',
+    'nl': 'Longitude',
+    'fr': 'Longitude',
+    'it': 'Longitude',
+    'es': 'Longitude',
+    'pl': 'Longitude',
+    'zh-cn': 'Longitude'
+  },
+  'declination': {
+    'en': 'Declination (°)',
+    'de': 'Neigung (°)',
+    'ru': 'Declination (°)',
+    'pt': 'Declination (°)',
+    'nl': 'Declination (°)',
+    'fr': 'Declination (°)',
+    'it': 'Declination (°)',
+    'es': 'Declination (°)',
+    'pl': 'Declination (°)',
+    'zh-cn': 'Declination (°)'
+  },
+  'azimuth': {
+    'en': 'Azimuth (0=S, 90=W, -90=E)',
+    'de': 'Ausrichtung (0=S, 90=W, -90=E)',
+    'ru': 'Azimuth (0=S, 90=W, -90=E)',
+    'pt': 'Azimuth (0=S, 90=W, -90=E)',
+    'nl': 'Azimuth (0=S, 90=W, -90=E)',
+    'fr': 'Azimuth (0=S, 90=W, -90=E)',
+    'it': 'Azimuth (0=S, 90=W, -90=E)',
+    'es': 'Azimuth (0=S, 90=W, -90=E)',
+    'pl': 'Azimuth (0=S, 90=W, -90=E)',
+    'zh-cn': 'Azimuth (0=S, 90=W, -90=E)'
+  },
+  'kwp': {
+    'en': 'kWp',
+    'de': 'kWp',
+    'ru': 'kWp',
+    'pt': 'kWp',
+    'nl': 'kWp',
+    'fr': 'kWp',
+    'it': 'kWp',
+    'es': 'kWp',
+    'pl': 'kWp',
+    'zh-cn': 'kWp'
+  },
+  'Plant settings': {
+    'en': 'Plant settings',
+    'de': 'Anlagedaten',
+    'ru': 'Plant settings',
+    'pt': 'Plant settings',
+    'nl': 'Plant settings',
+    'fr': 'Plant settings',
+    'it': 'Plant settings',
+    'es': 'Plant settings',
+    'pl': 'Plant settings',
+    'zh-cn': 'Plant settings'
+  }
 };
